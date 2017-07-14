166734356e72473465b7072df17474041d930944
Lukas Reineke <lukas.reineke@interone.de>
2017-07-14 18:17:54 +0200

    Cleaned up old mistakes

--- update.py
+++ update.py
@@ -11,84 +11,85 @@ import subprocess
 def cli(force, local):
     """
         updates all git modules in package.json\n
-        -f or --force to force reinstall, even if not out of date.
+        -f or --force to force reinstall, even if not out of date.\n
+        -l or --local to reinstall local dependencies
     """
 
-    try:
-        package = open('package.json')
-        searchItem = 'git+'
-        toInstall = ''
-        upToDate = False
+    gitSearchItem = 'git+'
+    localSearchItem = 'file:'
+    toInstall = ''
+    upToDate = False
 
-        for line in package:
-            if searchItem in line:
+    if not os.path.exists('package.json'):
+        click.echo('no package.json found')
+    else:
+        with open('package.json') as package:
+
+            if (local):
+                localLines = (line for line in package if localSearchItem in line)
+                for line in localLines:
+                    packageName = line.strip().split('"')
+                    toInstall += packageName[1] + ' '
+
+            gitLines = (line for line in package if gitSearchItem in line)
+            for line in gitLines:
                 packageName = line.strip().split('"')
 
                 if (force):
-                    toInstall = toInstall + packageName[1] + ' '
+                    toInstall += packageName[1] + ' '
                 else:
-                    split = packageName[3][4:].split(':')
-
-                    if split[0] == 'ssh':
-                        reformated = split[0] + ':' + split[1] + '/' + split[2]
+                    if not os.path.exists(os.path.join('node_modules', packageName[1], 'package.json')):
+                        toInstall += packageName[1] + ' '
                     else:
-                        reformated = packageName[3][4:]
 
-                    reformated = reformated.split('#')
-                    if (len(reformated) == 1):
-                        p = subprocess.Popen(
-                            [
-                                'git',
-                                'ls-remote',
-                                reformated[0],
-                                'HEAD'
-                            ],
-                            stdout=subprocess.PIPE,
-                            stderr=subprocess.PIPE
-                        )
-                    else:
-                        p = subprocess.Popen(
-                            [
-                                'git',
-                                'ls-remote',
-                                reformated[0],
-                                reformated[1]
-                            ],
-                            stdout=subprocess.PIPE,
-                            stderr=subprocess.PIPE
-                        )
+                        split = packageName[3][4:].split(':')
+                        if split[0] == 'ssh':
+                            reformated = split[0] + ':' + split[1] + '/' + split[2]
+                        else:
+                            reformated = packageName[3][4:]
 
-                    output, err = p.communicate()
-                    newestVersion = str(output).split("\t")[0]
+                        reformated = reformated.split('#')
+                        if (len(reformated) == 1):
+                            p = subprocess.Popen(
+                                [
+                                    'git',
+                                    'ls-remote',
+                                    reformated[0],
+                                    'HEAD'
+                                ],
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE
+                            )
+                        else:
+                            p = subprocess.Popen(
+                                [
+                                    'git',
+                                    'ls-remote',
+                                    reformated[0],
+                                    reformated[1]
+                                ],
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE
+                            )
 
-                    modulePackage = open(
-                        os.path.join(
-                            'node_modules', packageName[1], 'package.json'
-                        )
-                    )
-                    installedVersion = None
-                    for moduleLine in modulePackage:
-                        if 'resolved' in moduleLine:
-                            installedVersion = moduleLine.split('#')[1].split('"')[0]
+                        output, err = p.communicate()
+                        newestVersion = str(output).split("\t")[0]
 
-                    if (newestVersion != installedVersion):
-                        toInstall = toInstall + packageName[1] + ' '
-                    else:
-                        upToDate = True
-            if (local):
-                if 'file:' in line:
-                    packageName = line.strip().split('"')
-                    toInstall = toInstall + packageName[1] + ' '
+                        with open(os.path.join('node_modules', packageName[1], 'package.json')) as modulePackage:
+                            moduleLines = (moduleLine for moduleLine in modulePackage if 'resolved' in moduleLine)
+                            for moduleLine in moduleLines:
+                                installedVersion = moduleLine.split('#')[1].split('"')[0]
+                                if (newestVersion != installedVersion):
+                                    toInstall = toInstall + packageName[1] + ' '
+                                else:
+                                    upToDate = True
 
-
-        if (toInstall):
-            click.echo('installing:\n -- %s\n' % toInstall[:-1].replace(' ', '\n -- '))
-            os.system("npm i " + toInstall)
+    if (toInstall):
+        click.echo('installing:\n -- %s\n' % toInstall[:-1].replace(' ', '\n -- '))
+        os.system("npm i " + toInstall)
+    else:
+        if (upToDate):
+            click.echo('all git modules are up to date, nothing to do.')
         else:
-            if (upToDate):
-                click.echo('all git modules are up to date, nothing to do.')
-            else:
-                click.echo('no git modules in package.json')
+            click.echo('no git modules in package.json')
 
-    except:
-        click.echo('no package.json found')
